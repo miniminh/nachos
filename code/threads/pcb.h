74937b45bc14e0b158b18dcfdfb1db955e697d75
mtnkhang27 <maitrannguyenkhang.2704@gmail.com>
2024-04-18 19:51:53 +0700

    Update comment class pcb and ptable

--- code/threads/pcb.h
+++ code/threads/pcb.h
@@ -3,39 +3,39 @@
 
 class PCB {
    private:
-    Semaphore *joinsem;
-    Semaphore *exitsem;
-    Semaphore *multex;
+    Semaphore *joinsem;                 //Semaphore cho join
+    Semaphore *exitsem;                 //Semaphore cho exit
+    Semaphore *multex;                  //Semaphore cho truy xuất độc quyền
 
     int exitcode;
-    int numwait;
+    int numwait;                        //Số tiến trình đã join
 
     Thread *thread;
     char filename[128];
 
    public:
-    int parentID;
+    int parentID;                       //ID của tiến trình cha
     int processID;
     PCB();
     PCB(int id);
     ~PCB();
 
-    int Exec(char *filename, int pid);
-    int GetID();
-    int GetNumWait();
+    int Exec(char *filename, int pid);  //Tạo 1 thread mới có tên là filename và process là pid 
+    int GetID();                        //Trả về ProcessID của tiến trình gọi thực hiện
+    int GetNumWait();                   //Trả về số lượng tiến trình chờ
+    
+    void JoinWait();                    //Tiến trình cha đợi tiến trình con kết thúc
+    void ExitWait();                    //Tiến trình con kết thúc
+    void JoinRelease();                 //Báo cho tiến trình cha thực thi tiếp
+    void ExitRelease();                 //Cho phép tiến trình con kết thúc
 
-    void JoinWait();
-    void ExitWait();
-    void JoinRelease();
-    void ExitRelease();
+    void IncNumWait();                  //Tăng số tiến trình chờ
+    void DecNumWait();                  //Giảm số tiến trình chờ
 
-    void IncNumWait();
-    void DecNumWait();
+    void SetExitCode(int ec);           //Đặt exitcode của tiến trình
+    int GetExitCode();                  //Trả về exitcode
 
-    void SetExitCode(int ec);
-    int GetExitCode();
-
-    void SetFileName(char *fn);
+    void SetFileName(char *fn);         //Đặt tên của tiến trình
     char *GetFileName();
 };
 
--- code/threads/ptable.h
+++ code/threads/ptable.h
@@ -8,21 +8,23 @@
 
 class PTable {
    private:
-    Bitmap* bm;
-    PCB* pcb[MAX_PROCESS];
-    int psize;
-    Semaphore* bmsem;
+    Bitmap* bm;                         //đánh dấu các vị trí đã được sử dụng trong pcb
+    PCB* pcb[MAX_PROCESS];              
+    int psize;              
+    Semaphore* bmsem;                   //Dùng để ngăn chặn trường hợp nạp 2 tiến trình cùng lúc            
 
    public:
+   //khởi tạo size đối tượng PCB để lưu size process. Gán giá trị ban đầu là null
     PTable(int size);
-    ~PTable();
-    int ExecUpdate(char* name);
-    int ExitUpdate(int ec);
-    int JoinUpdate(int id);
-    int GetFreeSlot();
-    bool IsExist(int pid);
-    void Remove(int pid);
-    char* GetFileName(int id);
+    ~PTable();                          //Hủy
+    int ExecUpdate(char* name);         //Xử lí SC_Exec
+    int ExitUpdate(int ec);             //Xử lí SC_Exit
+    int JoinUpdate(int id);             //Xử lí SC_Join
+    int GetFreeSlot();                  //Tìm free slot để lưu thông tin cho tiến trình mới
+    bool IsExist(int pid);              //Kiểm tra tồn tại processID này không
+    void Remove(int pid);               //Khi tiến trình kết thúc, delete processID ra khỏi 
+    //mảng quản lý nó     
+    char* GetFileName(int id);          //Trả về tên tiến trình
 };
 
 #endif
